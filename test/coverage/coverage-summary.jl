55dc2c141032964938234c13967a3ea58517a3bd
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-09-09 21:13:48 -0500

    Add test/coverage/Project.toml, test/coverage/coverage-summary.jl

---
+++ test/coverage/coverage-summary.jl
@@ -0,0 +1,16 @@
+# This script has been copied from https://gitlab.com/tkpapp/GitlabJuliaDemo.jl/blob/master/test/coverage/coverage-summary.jl
+####
+#### Coverage summary, printed as "(percentage) covered".
+####
+#### Useful for CI environments that just want a summary (eg a Gitlab setup).
+####
+
+using Coverage
+
+cd(joinpath(@__DIR__, "..", "..")) do
+    processed = process_folder()
+    Codecov.submit_local(processed)
+    covered_lines, total_lines = get_summary(processed)
+    percentage = covered_lines / total_lines * 100
+    println("($(percentage)%) covered")
+end
